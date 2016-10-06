c1a9861cf7e1fbc636f680cbbb849f4afaf682a0
Linley <linleyjfaulkner@gmail.com>
2016-10-06 20:06:54 +0000

    add updated assignment 1.5

---
+++ callback.js
@@ -0,0 +1,16 @@
+var arr = ["Alice", "Bob", "Waldo", "Winston"]
+
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == "Waldo") {
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound() {
+
+  console.log("Waldo is found at index " + arr.indexOf("Waldo") + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
